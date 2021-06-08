faff910a63717ada5bf2bfd84da910360cabfbad
ORiveraJr84 <oriverajr84@gmail.com>
2021-06-08 13:24:23 -0400

    WIP

--- car/car.js
+++ car/car.js
@@ -1,1 +1,15 @@
 // Build a Car class!
+// Car class to have drive() and asyncDrive() methods, make, model, and odometer properties.
+class Car {
+  constructor(make, model, odometer) {
+    this.make = make;
+    this.model = model;
+    this.odometer = 0;
+  }
+
+  drive(...numMiles) {
+    this.odometer += numMiles.reduce((acc, curr) => acc + curr, 0);
+  }
+}
+
+module.exports = Car;
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,51 @@
 // Test away!
+const Car = require("./car");
+
+describe("Car class", () => {
+  let car;
+  beforeEach(() => {
+    car = new Car("toyota", "prius");
+  });
+
+  describe("constructor", () => {
+    it("has a make property", () => {
+      expect(car.make).toEqual("toyota");
+    });
+
+    it("has a model property", () => {
+      expect(car.model).toEqual("prius");
+    });
+
+    it("has an odometer property", () => {
+      expect(car.odometer).toBe(0);
+    });
+  });
+
+  describe("has drive() method", () => {
+    it("method exists", () => {
+      expect(car.drive).toBeDefined();
+    });
+    it("is a function", () => {
+      expect(car.drive).toBeInstanceOf(Function);
+    });
+    it("changes odometer", () => {
+      expect(car.odometer).toBe(0);
+      car.drive(4);
+      expect(car.odometer).toBe(4);
+    });
+    it("works multiple times", () => {
+      expect(car.odometer).toBe(0);
+      car.drive(4);
+      expect(car.odometer).toBe(4);
+      car.drive(5);
+      expect(car.odometer).toBe(9);
+      car.drive(6);
+      expect(car.odometer).toBe(15);
+    });
+    it("accepts multiple arguments", () => {
+      expect(car.odometer).toBe(0);
+      car.drive(4, 5, 6);
+      expect(car.odometer).toBe(15);
+    });
+  });
+});
